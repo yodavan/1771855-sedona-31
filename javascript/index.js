b4a41830c5d42155d7cf3bba54c6aede79eefd16
yodavan <84133554+yodavan@users.noreply.github.com>
2021-07-04 13:42:48 +0300

    Внес исправления для FF

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ const link = document.querySelector(".search__button");
 const popup = document.querySelector(".modal");
 const form = document.querySelector("form");
 
-link.addEventListener("click", function() {
+link.addEventListener("click", function(evt) {
   popup.classList.add("modal-show");
 });
 form.addEventListener("submit", function(evt) {
