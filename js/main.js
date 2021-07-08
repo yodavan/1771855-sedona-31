3061bbba9cc6da8819a4ad61fc5cc7a7ace413ac
yodavan <84133554+yodavan@users.noreply.github.com>
2021-07-08 19:12:07 +0300

    Доступная разметка внутренней страницы
    Поправил все замечания

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ const form = document.querySelector("form");
 const checkindate = document.querySelector("[name=check-in-date]");
 const checkoutdate = document.querySelector("[name=check-out-date]");
 const adult = document.querySelector("[name=adult]");
-const openpopup = document.querySelector (".open-popup");
+const openpopup = document.querySelector (".search__form");
 
+popup.classList.add("modal-close");
 link.addEventListener("click", function(evt) {
   popup.classList.add("modal-show");
 });
+openpopup.classList.add("open-popup");
 form.addEventListener("submit", function(evt) {
   if (!checkindate.value || !checkoutdate.value || !adult.value) {
     evt.preventDefault();
